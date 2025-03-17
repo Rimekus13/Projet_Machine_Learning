4fd0de21055e22e369cc017dd9b4fbf05e9d0910
SULFYY <70634014+ChTheo@users.noreply.github.com>
2025-03-17 16:47:31 +0100

    ajout de 3 score d'evaluation

--- src/scripts/evaluation.py
+++ src/scripts/evaluation.py
@@ -15,3 +15,37 @@ def cross_validate_model(model, X, y, cv=5):
     """Effectue une validation croisée."""
     scores = cross_val_score(model, X, y, cv=cv)
     return scores.mean()
+
+def eval_silouhette(df_test, predictions):
+    return silhouette_score(df_test, predictions)
+
+
+def eval_davies_bouldin(X, labels):
+    """
+    Fonction qui prend un set de données (X) et des predictions (labels),
+    calcule l'indice de Davies-Bouldin et l'affiche.
+
+    :param model: Un modèle de clustering (ex : KMeans)
+    :param X: Un tableau de données (matrice de caractéristiques)
+    """
+    # Calculer l'indice de Davies-Bouldin
+    db_index = davies_bouldin_score(X, labels)
+
+    # Afficher l'indice
+    print(f"L'indice de Davies-Bouldin est: {db_index}")
+
+
+def calculer_et_afficher_calinski_harabasz(X, labels):
+    """
+    Fonction qui prend un set de données (X) et des predictions (labels),
+    calcule l'indice de Calinski-Harabasz et l'affiche.
+
+    :param model: Un modèle de clustering (ex : KMeans)
+    :param X: Un tableau de données (matrice de caractéristiques)
+    """
+
+    # Calculer l'indice de Calinski-Harabasz
+    ch_index = calinski_harabasz_score(X, labels)
+
+    # Afficher l'indice
+    print(f"L'indice de Calinski-Harabasz est: {ch_index}")
