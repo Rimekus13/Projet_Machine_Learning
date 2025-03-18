bcc7fb70b36ac69a1117138c6c74234def42a984
SULFYY <70634014+ChTheo@users.noreply.github.com>
2025-03-18 10:52:30 +0100

    fonciton d'export des wordcloud

--- src/scripts/visualize.py
+++ src/scripts/visualize.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+import pandas as pd
+from wordcloud import WordCloud
+from preprocessing import get_categorical
+
 
 def mean_column_per_cluster(labels,dataset,cluster_id=1):
 
@@ -14,3 +18,28 @@ def mean_column_per_cluster(labels,dataset,cluster_id=1):
     plt.ylabel('Moyenne')
     plt.title(f'Moyennes des caractéristiques pour le cluster {cluster_id}')
     plt.show()
+
+
+def word_cloud(df):
+
+    output_folder = "../../image/kmeans_1000"
+    # Récupérer les colonnes catégorielles du DataFrame
+    df_cat = df.select_dtypes(exclude=['number'])
+
+    # Concaténer toutes les valeurs textuelles en une seule chaîne par colonne
+    columns_values_list = {col: ' '.join(df_cat[col].astype(str)) for col in df_cat.columns}
+
+    # Créer un nuage de mots pour chaque colonne catégorielle
+    for col, text in columns_values_list.items():
+        # Générer le nuage de mots pour la colonne
+        wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
+
+        # Afficher le nuage de mots
+        plt.figure(figsize=(10, 5))
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.title(f'Nuage de mots pour la colonne: {col}', fontsize=16)
+        plt.axis('off')  # Masquer les axes
+        plt.show()
+        filename = f"{output_folder}/wordcloud_kmeans_1000_{col}.png"
+        plt.savefig(filename)
+        plt.close()
